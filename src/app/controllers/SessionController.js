6bb9d13b84482c8afec72363b6047351e907e5a4
Paulo Henrique <paulohfcmendes@gmail.com>
2020-05-17 14:59:46 -0300

    feat/ jwt auth

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -11,6 +11,46 @@ class SessionController {
       email: Yup.string().email().required(),
       password: Yup.string().required(),
     });
+
+    if (!(await schema.isValid(req.body))) {
+      return res
+        .status(401)
+        .json({ message: "Erro na validação.\nVerifique seus dados." });
+    }
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({
+        where: { email },
+      });
+
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "Usuário não encontrado.\nVerifique seus dados!" });
+      }
+
+      if (!(await user.checkPassword(password))) {
+        return res.status(401).json({ message: "Senha incorreta." });
+      }
+
+      const { id, name, phone, address } = user;
+
+      return res.json({
+        user: {
+          id,
+          name,
+          email,
+          phone,
+          address,
+        },
+        token: jwt.sign({ id }, auth.secret, {
+          expiresIn: auth.expiresIn,
+        }),
+      });
+    } catch (err) {
+      return res.json({ message: `Erro no servidor! ${err}` });
+    }
   }
 }
 
